50c91a75fd86d32551111d5b6c03889979fcbfb7
arthur <arthur.rios007@gmail.com>
2024-02-25 23:11:59 -0300

    chore: add font and theme

---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  input:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.COLORS.BLUE};
+  }
+
+  body {
+    background: ${({ theme }) => theme.COLORS.BASE_BACKGROUND};
+    color: ${({ theme }) => theme.COLORS.BASE_TEXT};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    ${({ theme }) => theme.FONTS.TEXT_M}
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+
+    &:focus {
+      border: none;
+    }
+  }
+
+  a:focus {
+    outline: none;
+    cursor: pointer;
+  }
+`
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,56 @@
+import { css } from 'styled-components'
+
+export const defaultTheme = {
+  COLORS: {
+    BLUE: '#3294F8',
+    BASE_TITLE: '#E7EDF4',
+    BASE_SUBTITLE: '#C4D4E3',
+    BASE_TEXT: '#AFC2D4',
+    BASE_SPAN: '#7B96B2',
+    BASE_LABEL: '#3A536B',
+    BASE_BORDER: '#1C2F41',
+    BASE_POST: '#112131',
+    BASE_PROFILE: '#0B1B2B',
+    BASE_BACKGROUND: '#071422',
+    BASE_INPUT: '#040F1A',
+  } as const,
+
+  FONTS: {
+    TITLE_L: css`
+      font-family: 'Nunito', sans-serif;
+      font-size: 1.5rem;
+      line-height: 130%;
+      font-weight: 700;
+    `,
+    TITLE_M: css`
+      font-family: 'Nunito', sans-serif;
+      font-size: 1.25rem;
+      line-height: 160%;
+      font-weight: 700;
+    `,
+    TITLE_S: css`
+      font-family: 'Nunito', sans-serif;
+      font-size: 1.125rem;
+      line-height: 160%;
+      font-weight: 700;
+    `,
+    TEXT_M: css`
+      font-family: 'Nunito', sans-serif;
+      font-size: 1rem;
+      line-height: 160%;
+      font-weight: 400;
+    `,
+    TEXT_S: css`
+      font-family: 'Nunito', sans-serif;
+      font-size: 0.875rem;
+      line-height: 160%;
+      font-weight: 400;
+    `,
+    LINK: css`
+      font-family: 'Nunito', sans-serif;
+      font-size: 0.75rem;
+      line-height: 160%;
+      font-weight: 700;
+    `,
+  } as const,
+}
